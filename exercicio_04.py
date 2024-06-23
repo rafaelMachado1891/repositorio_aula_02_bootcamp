b394301cb29d00a2f410737fcfa9f57e583a75a1
rafaelMachado1891 <rafadmn1@gmail.com>
2024-06-23 10:09:58 -0300

    resolucao exercicio_04

---
+++ exercicio_04.py
@@ -0,0 +1,28 @@
+# #### Booleanos (`bool`)
+
+# 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+valor1 = True
+valor2 = False
+resultado_and = valor1 and valor2
+print("Resultado do AND lógico:", resultado_and)
+# 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+valor1 = True
+valor2 = False
+resultado_or = valor1 or valor2
+print("Resultado do or lógico:", resultado_or)
+
+# 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+valor1 = input('insira uma valor: ')
+resultado_not = not valor1
+print(resultado_not)
+# 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+numero1 = input('insira o primeiro numero: ')
+numero2 = input('insira o segundo numero aqui: ')
+resultado_igualdade = numero1 == numero2
+print(resultado_igualdade)
+# 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+numero1 = input('insira o primeiro numero: ')
+numero2 = input('insira o segundo numero aqui: ')
+resultado_igualdade = numero1 != numero2
+print(resultado_igualdade) 
+
