3a55fb002cbd1d206fcc37b938426ec6f958c60f
rafaelMachado1891 <rafadmn1@gmail.com>
2024-06-24 23:02:19 -0300

    resolucao do desafio aula 2

--- desafio_01.py
+++ desafio_01.py
@@ -10,14 +10,29 @@ elif nome.isspace():
     print('você digitou somente espaco')
     exit()
 
-salario = float(input("Digite o seu salário: "))
+try:
+    salario = float(input("Digite o seu salário: "))
+    if salario < 0:
+        print('insira uma valor valido para o salário')
+except: 
+    ValueError
+    print('Você inseriu o seu salario errado, digite um valor valido')
+    exit()
 
+try: 
+    bonus_valor = float(input("Qual foi o valor do seu bonus? "))
 
+    if bonus_valor < 0:
+     print('Insira um valor positivo para o bonus')
 
-bonus_valor = float(input("Qual foi o valor do seu bonus? "))
+except:
+    ValueError
+    print('insira um valor decimal para o bônus')
+    NameError
+    print('você inseriu um valor invalido para o bonus, digite uma valor no formato decimal separado por "." ')
+    exit()
 
 bonus = 1000 + salario * bonus_valor
-
-print("Seja bem vindo ",nome)
+ 
 
 print("O valor total do seu bonus é: ", bonus)
